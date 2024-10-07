d4d6b025a7d07a2c78bdde6cd227190e5cd0c25b
Prof Gesinelly <106716290+ProfGesinelly@users.noreply.github.com>
2024-10-07 18:02:02 +0000

    codigo reunião 4

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,16 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    //console.log(dados)
+
+    const paragrafo = document.createElement("p")
+    paragrafo.classList.add("graficos-container__texto")
+
+    paragrafo.innerHTML = 
+        'Você sabia que o mundo tem $(dados.total_pessoas_mundo)bilhões de pessoas que aproximadamente 5.04 bilhões estão conectadas em alguma rede social e passam em média 2 horas e 38 minutos conectadas. Isso significa que aproximadamente 63.89% de pessoas conectadas em alguma rede social.'
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+
 }
 
 vizualizarInformacoesGlobais()
