b2abd3343df1b802c960273760c8d366aefdc1a6
Prof Gesinelly <106716290+ProfGesinelly@users.noreply.github.com>
2024-10-21 17:53:56 +0000

    atualizado

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,16 +3,13 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    //console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
 
-    const paragrafo = document.createElement("p")
-    paragrafo.classList.add("graficos-container__texto")
-
-    paragrafo.innerHTML = 
-        'Você sabia que o mundo tem $(dados.total_pessoas_mundo)bilhões de pessoas que aproximadamente 5.04 bilhões estão conectadas em alguma rede social e passam em média 2 horas e 38 minutos conectadas. Isso significa que aproximadamente 63.89% de pessoas conectadas em alguma rede social.'
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-
 }
 
 vizualizarInformacoesGlobais()
