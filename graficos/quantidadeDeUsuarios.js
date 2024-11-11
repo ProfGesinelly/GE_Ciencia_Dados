072cd39a970c51bbd5801fe1008c01b593f9cebd
Prof Gesinelly <106716290+ProfGesinelly@users.noreply.github.com>
2024-11-11 20:48:42 +0000

    atualizado

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,28 +1,60 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
-async function quantidadesUsuarios(){
+async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    
+    const rest = await fetch(url)
+    const dados = await rest.json()
+
     const nomeDasRedes = Object.keys(dados)
-    const quantidadesUsuarios = Object.values(dados)
+    const quantidadeUsuarios= Object.values(dados)
 
     const data = [
         {
-            x : nomeDasRedes,
-            y : quantidadesUsuarios,
-            type: 'bar'
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
             }
         }
     ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: "Redes sociais com mais usuários no mundo",
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30,
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Nome das redes sociais",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: "Bilhoes de usuários ativos",
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        }
+    }
     
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 }
-
-quantidadesUsuarios();
+ 
+quantidadeUsuarios()
